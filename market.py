61abc187e99fdc6383d7554d52b77e39886ee73d
krbysh <krbysh@krbysh.net>
2021-09-29 23:58:11 +0900

    Refactoring and integrated with Twitter

--- market.py
+++ market.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python3
+# condig: utf-8
+
 import sys
 import pyocr
 import pyocr.builders
@@ -5,54 +8,78 @@ import pyautogui
 import pyperclip
 import numpy as np
 import dropbox
+import os
+import tweepy
 
 from subprocess import Popen, PIPE
 from time import sleep, time
 from PIL import Image
 from discordwebhook import Discord
+from os.path import join,dirname
+from dotenv import load_dotenv
+
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
 
 itemlist = [
-#	"スモーキーカード",
-#	"エクリプスカード",
+	"スモーキーカード",
+	"エクリプスカード",
+	"エクリプス★カード",
 	"マスターリングカード",
-#	"バジルリスクカードカード",
-#	"ボーカルカード",
+	"マスターリング★カード",
+	"バジルリスクカード",
+	"ボーカルカード",
 	"ゴーストリングカード",
-#	"トードカード",
-#	"ロータージャイロカード",
+	"トードカード",
+	"トード★カード",
+	"ロータージャイロカード",
+	"ロータージャイロ★カード",
 	"ドラゴンフライカード",
-#	"さすらい狼カード",
+	"ドラゴンフライ★カード",
+	"さすらい狼カード",
+	"さすらい狼★カード",
 	"ウッドゴブリンカード",
-#	"アヌビスカード",
-#	"グリフォンカード",
-#	"ヒェグンカード",
+	"ウッドゴブリン★カード",
+	"アヌビスカード",
+	"アヌビス★カード",
+	"グリフォンカード",
+	"グリフォン★カード",
+	"ヒェグンカード",
+	"ヒェグン★カード",
 	"オークベイビーカード",
-#	"ジャックカード",
-#	"ミュータントドラゴンカード",
-#	"ラフレシアカード",
-#	"オウルデュークカード",
-#	"アリスカード",
-#	"ジルタスカード",
-#	"ミステルテインカード",
-#	"ダークイリュージョンカード",
-#	"ひ弱な操り人形カード",
-#	"ビッグベンカード",
-#	"時計塔管理者カード",
+	"ジャックカード",
+	"ミュータントドラゴンカード",
+	"ミュータントドラゴン★カード",
+	"ラフレシアカード",
+	"ラフレシア★カード",
+	"オウルデュークカード",
+	"アリスカード",
+	"アリス★カード",
+	"ジルタスカード",
+	"ミステルテインカード",
+	"ミステルテイン★カード",
+	"ダークイリュージョンカード",
+	"ダークイリュージョン★カード",
+	"ビッグベンカード",
+	"時計塔管理者カード",
+	"時計塔管理者★カード",
 	"チェペットカード",
-#	"炎の魔女カード",
-#	"パイドパイパーカード",
-#	"イグニゼム＝セニアカード",
-#	"アルマイア=デュンゼカード",
-#	"ロリルリカード",
-#	"ゲイズティカード",
+	"炎の魔女カード",
+	"炎の魔女★カード",
+	"パイドパイパーカード",
+	"イグニゼム・セニア（MINI）カード",
+	"アルマイア・デュンゼ（MINI）カード",
+	"ロリルリカード",
+	"ロリルリ★カード",
+	"ゲイズティカード",
 	"狂暴化したガリオンカード",
-#	"堕ちた大神官ヒバムカード",
-#	"妖狐ミョグェカード",
-#	"カエデの精霊イシュマカード",
-#	"銀魚湖のヌシカード",
-#	"火焔鳥ジュリーカード",
-#	"リムルカード",
-#	"クリスタルビースト",
+	"堕ちた大神官ヒバムカード",
+	"妖狐ミョグェカード",
+	"カエデの精霊イシュマカード",
+	"銀魚湖のヌシカード",
+	"火焔鳥ジュリーカード",
+	"リムルカード",
+	"クリスタルビースト",
 	"ヴァルキリー・ラーズグリーズカード",
 	"アーケインキューブカード",
 	"思念集合体カード",
@@ -135,9 +162,9 @@ itemlist = [
 	"アークエンジェリングカード",
 	"ロードオブデスカード",
 	"ブラッディマーダーカード",
-	"カトリーヌ＝ケイロンカード",
-	"セシル＝ディモンカード",
-	"エレメス＝ガイルカード",
+	"カトリーヌ・ケイロン（MVP）カード",
+	"セシル・ディモン（MVP）カード",
+	"エレメス・ガイル（MVP）カード",
 	"ウルフおばあちゃんカード",
 	"クトルラナックスカード",
 	"ヒルウィンドカード",
@@ -150,31 +177,40 @@ itemlist = [
 	"魂の奏者カード"
 ]
 
-# nirvana_rom
-discord_url = "https://discord.com/api/webhooks/887696502821105664/CyiKjOX-c8I6BLY2PGmHgJMqLHGgsSEv2zaHsjSQIOITqPGzqAfRblSoqttC-UNHgWSc"
-# krbysh
-# discord_url = "https://discord.com/api/webhooks/887372286586413098/lDES7A_xl5GVr0ebklUpTV_8rUZvNSB09Hwl6-dLNAoq1nRVzJxIad-yi886mfF0DMPh"
+CK = os.getenv('CK')
+CS = os.getenv('CS')
+AT = os.getenv('AT')
+AS = os.getenv('AS')
+
+url_media = "https://upload.twitter.com/1.1/media/upload.json"
+url_text = "https://api.twitter.com/1.1/statuses/update.json"
+
+discord_url = os.getenv('KRBYSH_URL')
 discord_headers = {'Content-Type': 'application/json'}
+
+auth = tweepy.OAuthHandler(CK, CS)
+auth.set_access_token(AT, AS)
 
 class TransferData:
     def __init__(self, access_token):
         self.access_token = access_token
 
     def upload_file(self, file_from, file_to):
-        """upload a file to Dropbox using API v2
-        """
         dbx = dropbox.Dropbox(self.access_token)
 
         with open(file_from, 'rb') as f:
             dbx.files_upload(f.read(), file_to, mode=dropbox.files.WriteMode.overwrite)
 
-access_token = 'cLCjFHhr-zsAAAAAAAAAAQh1y5kxdh1T_pqAm-kjGISri82aiEFngKpy11gy8fIF'
-transferData = TransferData(access_token)
+# dp_token = 'cLCjFHhr-zsAAAAAAAAAAQh1y5kxdh1T_pqAm-kjGISri82aiEFngKpy11gy8fIF'
+dp_token = os.getenv('DP_TOKEN')
+
+transferData = TransferData(dp_token)
 
 file_from = 'Avatar.png'
 file_to = '/Avatar.png'  
 
-dp_url="https://dl.dropboxusercontent.com/s/4ps95y182sbctrp/Avatar.png"
+# dp_url="https://dl.dropboxusercontent.com/s/4ps95y182sbctrp/Avatar.png"
+dp_url = os.getenv('DP_URL')
 
 tools = pyocr.get_available_tools()
 if len(tools) == 0:
@@ -188,25 +224,6 @@ print("Available languages: %s" % ", ".join(langs))
 lang = langs[0]
 print("Will use lang '%s'" % (lang))
 
-def PosGet():
-	# 3秒待ってからカーソル位置の座標を取得
-	print("左上隅の座標を取得します")
-	sleep(3)
-	x1, y1 = pyautogui.position()
-	print(str(x1) + "," + str(y1))
-
-	# 3秒待ってからカーソル位置の座標を取得
-	print("右下隅の座標を取得します")
-	sleep(3)
-	x2, y2 = pyautogui.position()
-	print(str(x2) + "," + str(y2))
-
-	# PyAutoGuiのregionの仕様のため、相対座標を求める
-	x2 -= x1
-	y2 -= y1
-
-	return(x1, y1, x2, y2)
-
 def MarketSearch(Item):
 	pyautogui.moveTo(235,225)  # search Window
 	pyautogui.click()
@@ -217,11 +234,14 @@ def MarketSearch(Item):
 	sleep(1)
 	pyautogui.hotkey('command','v')
 	pyautogui.hotkey('enter')
+	pyautogui.hotkey('enter')
 	pyautogui.moveTo(695,355)  # searchResult Window
 	pyautogui.click()
 
 	sleep(1)
-	sc = pyautogui.screenshot('MarketResult.png',region=(443, 210, 405, 112))
+	# 20210825 
+	# sc = pyautogui.screenshot('MarketResult.png',region=(443, 210, 405, 112))
+	sc = pyautogui.screenshot('MarketResult.png',region=(443, 203, 405, 112))
 	im = Image.open(r"MarketResult.png").convert("RGB")
 	im.crop((4, 8, 103, 106)).save("Avatar.png")
 	price = im.crop((220, 60, 400, 90))
@@ -243,7 +263,7 @@ def OptimizePrice(image):
 	return Image.fromarray(arr)
 
 def OptimizeTime(image):
-	border = 165
+	border = 170
 	arr = np.array(image)
 	for i in range(len(arr)):
 		for j in range(len(arr[i])):
@@ -259,30 +279,25 @@ def TranslationActors(img):
 	txt = tool.image_to_string(
 		Image.open(img),
 		lang="eng",
-		builder=pyocr.builders.TextBuilder(tesseract_layout=4)
+		builder=pyocr.builders.TextBuilder(tesseract_layout=1)
 	)
-	print(txt.replace("\n",""))
+	# print(txt.replace("\n",""))
 	return(txt)
 
 if __name__ == "__main__":
 	p = ""
 	t = ""
+	twitter = tweepy.API(auth)
 	discord = Discord(url=discord_url)
-	sleep(5)
-
-#	while True:		
+	sleep(5)	
 	for item in itemlist:
 		MarketSearch(item)
 
 		p = TranslationActors("Price.png").replace('.',",")
-		t = TranslationActors("Time.png").replace('.|;|-',":")
+		t = TranslationActors("Time.png").replace('-',":")
 		
-		if p is not "" :
+		if p is not "" and t is not "":
 			transferData.upload_file(file_from, file_to)
 			sleep(3)
-
-			if t is not "":
-				discord.post(content=p + " z @" + t, username=item, avatar_url=dp_url + "?" + str(time()))
-			#else:
-			#	discord.post(content=p + " z", username=item, avatar_url=dp_url + "?" + str(time()))
-#		sleep(3600)
+			discord.post(content=p + " z @" + t, username=item, avatar_url=dp_url + "?" + str(time()))
+			twitter.update_status_with_media(status = item + ": " + p + " z 残り" + t, filename = "MarketResult.png")
